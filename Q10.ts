fe16f2d623addd8768aebb9eb134781af8eae81d
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 02:54:42 +0500

    NODEJS Assignments Q10
    10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.

---
+++ Q10.ts
@@ -0,0 +1,11 @@
+// 9. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Addition promram
+let a: number = 10;
+let b: number = 20;
+console.log(`${a}+${b}=${a + b}`);
+// subtraction promram
+console.log(`${b}-${a}=${b - a}`);
+// multiplication
+console.log(`${a}*${b}=${a * b}`);
+// division
+console.log(`${b}/${a}=${b / a}`);
